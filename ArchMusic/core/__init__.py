2f78f7d67c7a6b69ecd976d6d30f266615cbe1ca
zuchzub <kumsalmuzik07@gmail.com>
2025-10-24 15:47:15 +0300

    Update __init__.py

--- ArchMusic/core/__init__.py
+++ ArchMusic/core/__init__.py
@@ -1,6 +1,11 @@
+import os
 import asyncio
-try:
-    import uvloop  # type: ignore
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-except Exception:
-    pass
+
+# İstersen uvloop'u kapat: DISABLE_UVLOOP=true
+if os.getenv("DISABLE_UVLOOP", "").lower() not in ("1", "true", "yes"):
+    try:
+        import uvloop  # type: ignore
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    except Exception:
+        # Uyumlu değilse sessizce varsayılana düş
+        pass
