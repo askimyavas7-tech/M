e3cfc4dbed434e9f7b2bb608a3e2d4000c96e328
askimyavas7-tech <askimyavas7@gmail.com>
2025-10-25 16:30:25 +0300

    Update git.py

--- ArchMusic/core/git.py
+++ ArchMusic/core/git.py
@@ -1,8 +1,5 @@
-import subprocess
-def fetch_updates(repo_url: str) -> None:
-    try:
-        subprocess.run(["git", "fetch", "--all"], check=False)
-        subprocess.run(["git", "pull", "--no-edit", repo_url], check=False)
-        print(f"[INFO] - ArchMusic.core.git - Fetched Updates from: {repo_url}")
-    except Exception as e:
-        print("[WARN] - ArchMusic.core.git - Git fetch skipped:", e)
+# ArchMusic/core/git.py
+
+def fetch_updates(repo_url=None):
+    # Git updater pasif edildi çünkü Heroku git komutu desteklemiyor.
+    return
